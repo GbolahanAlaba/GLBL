05db1d2fc1db1b139b6a90506cbd5f328a5003d4
GbolahanAlaba <thegbolahanalaba@gmail.com>
2022-08-05 17:06:09 +0100

    First

--- signin.py
+++ signin.py
@@ -72,12 +72,9 @@ class Signin:
 
         UsernameEntry = Entry(SigninFrame, font=('roboto', 10, 'bold'), width=22, bd=2, relief='groove')
         UsernameEntry.grid(row=0, column=1, padx=10, pady=10)
-        # UserEntry.insert(1, 'Enter Your Username')
-
 
         Password = Label(SigninFrame, text='Password', font=('roboto', 11, 'bold'), bg='#f7f3f2', fg='#bd2505')
         Password.grid(row=1, column=0, padx=20, pady=10)
-
 
         PasswordEntry = Entry(SigninFrame, font=('roboto', 10, 'bold'), show='*', width=22, bd=2, relief='groove')
         PasswordEntry.grid(row=1, column=1, padx=10, pady=10)
@@ -86,18 +83,14 @@ class Signin:
         ChkButton = Checkbutton(SigninFrame, text='Remember Me', bg='#f7f3f2', cursor='hand2')
         ChkButton.grid(row=2, column=0, pady=5)
 
-
         SigninButton = Button(SigninFrame, text='Sign in', font=('roboto', 10, 'bold'), bg='#bd2505', fg='white', command=signin)
         SigninButton.place(x=150, y=93)
-
 
         ClearButton = Button(SigninFrame, text='Clear', font=('roboto', 10, 'bold'), bg='#bd2505', fg='white', command=clear)
         ClearButton.place(x=230, y=93)
 
-
         ForgetPwLabel = Button(SigninFrame, text='Forget Password?', font=('roboto', 8, 'bold', 'underline'), bg='#f7f3f2', fg='#1a3783', bd=0, cursor='hand2')
         ForgetPwLabel.place(x=10, y=130)
-
 
         SignupLabel = Button(SigninFrame, text='Create Account', font=('roboto', 8, 'bold', 'underline'), bg='#f7f3f2', fg='#1a3783', bd=0, cursor='hand2', command=self.create)
         SignupLabel.place(x=130, y=130)
